a8dd5a3f8646642a72e8a5dcb957884b9b259db7
river-ceanne <snvencer@gmail.com>
2019-03-27 22:52:50 -0700

    addition of flags in questions 6 & 7

--- app.js
+++ app.js
@@ -4,20 +4,15 @@
 console.log("JS Script linked/started.");
 
 var name;
+var rightAnswersNum = 0;
 var reply;
 var yesReply = ["y","yes","true"];
 var noReply = ["n","no","false"];
 var visitedCountries = ["philippines","grand cayman","australia","france","dubai"];
-
-var qAndA = [
-    ["Am I girl?",yesReply], //0
-    ["Do I like sushi?",yesReply],//1
-    ["Am I a coder?",yesReply],//2
-    ["Am I an Amazon employee?",yesReply],//3
-    ["Do I play an instrument?",yesReply],//4
-    ["Guess the number I'm thinking:",22],//5
-    ["Guess a country I have visited:", visitedCountries]//6
-];
+var countryTries = 6;
+var countryGuessed = false;
+var numberTries = 4;
+var numberGuessed = false;
 
 var questions = [
     "Am I girl?",
@@ -25,8 +20,8 @@ var questions = [
     "Am I a coder?",
     "Am I an Amazon employee?",
     "Do I play an instrument?",
-    "Guess the number I'm thinking:",
-    "Guess a country I have visited:"
+    "Guess the number I'm thinking:", //4 tries
+    "Guess a country I have visited:" // 6 tries
 ];
 
 var answers = [
@@ -45,7 +40,7 @@ if(name == null || name === ""){
 }
 alert("Welcome " + name + "! Let's proceed!");
 
-for(var i = 0; i < questions.length; i++){
+for(var i = 0; i < 5; i++){
     reply = prompt(questions[i]).toLowerCase();
     var counter = 0;
     var answerArr = answers[i];
@@ -53,6 +48,7 @@ for(var i = 0; i < questions.length; i++){
         if(reply === answerArr[counter] || reply === answers[i]){
             console.log("Correct answer was submitted");
             alert('You are right!!!');  
+            rightAnswersNum++;
             break;
         }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
             console.log("Wrong answer was submitted");
@@ -61,127 +57,65 @@ for(var i = 0; i < questions.length; i++){
 
         counter++;
     }
-
-    
-
 }
 
-/*
+while(numberTries != 0){
+    reply = prompt(questions[5]).toLowerCase();
+    var counter = 0;
+    var answerArr = answers[5];
+    while(counter < answerArr.length){
+        if(reply === answerArr[counter] || reply == answers[5]){
+            console.log("Correct answer was submitted");
+            alert('You are right!!!');  
+            rightAnswersNum++;
+            numberGuessed = true;
+            break;
+        }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
+            console.log("Wrong answer was submitted");
+            alert("Your answer is . . wrong! Try Again!");
+        }
 
-for(var i = 0; i < questions.length; i++){
-    
-    for(var ii = 0; ii < qAndA[i].length;ii++){
+        counter++;
+    }
 
-        answer = prompt(qAndA[i][0]).toLowerCase();
+    if(numberGuessed){
+        break;
+    }
 
-        var answerAr = qAndA[i][ii];
-        var answerCounter = 0;
+    numberTries--;
+    if(numberTries == 0){
+        alert("You get no more tries for this question! Sorry T_T");
+    }
+}
 
-        if(i < 5){//question upto 5 -- yes/no
+while(countryTries != 0){
+    reply = prompt(questions[6]).toLowerCase();
+    var counter = 0;
+    var answerArr = answers[6];
+    while(counter < answerArr.length){
+        if(reply === answerArr[counter] || reply === answers[5]){
+            console.log("Correct answer was submitted");
+            alert('You are right!!!');  
+            rightAnswersNum++;
+            countryGuessed = true;
+            break;
+        }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
+            console.log("Wrong answer was submitted");
+            alert("Your answer is . . wrong! Try again!");
+        }
 
-            while(answerCounter < answerAr.length){
-                if(answer === answerAr[answerCounter]){
-                    console.log("Correct answer was submitted");
-                    alert('You are right!!!');  
-                }
-            }
-           
-        }//////////////////////////////////////BORDER END OF IF
+        counter++;
+    }
 
-        if(i == 5){//guessing a number question
+    if(countryGuessed){
+        break;
+    }
 
-            for(var iii = 4; iii > 0; iii++){
-                      
-            }
-            
-
-
-        }/////////////////////////////////////BORDER END OF IF
-
-        if(i == 6){//guessing -- with multiple possible answers
-            
-        }///////////////////////////////////////BORDER END OF IF
-
-        
-    }//end of for loop checking for answers
-
-}//end of for loop prompt each question
-
-/*
-//Am I a girl?
-var answer = prompt(questions[0]).toLowerCase();
-if(answer != null || answer != ""){
-    if(answer === "y" || answer === "yes"){
-        console.log("Correct answer was submitted");
-        alert('You are right!!!');  
-    }else if(answer === "n" || answer === "no"){
-        console.log("Wrong answer was submitted");
-        alert("Your answer is . . wrong!");
-    }else{
-        console.log("Invalid Answer");
-        alert("Your answer was invalid.");
+    countryTries--;
+    if(countryTries == 0){
+        alert("You get no more tries for this question! Sorry T_T");
     }
 }
 
 
-//Do I like Sushi?
-var answer = prompt(questions[1]).toLowerCase();
-if(answer != null || answer != ""){
-    if(answer === "y" || answer === "yes"){
-        console.log("Correct answer was submitted");
-        alert('You are right!!!');  
-    }else if(answer === "n" || answer === "no"){
-        console.log("Wrong answer was submitted");
-        alert("Your answer is . . wrong!");
-    }else{
-        console.log("Invalid Answer");
-        alert("Your answer was invalid.");
-    }
-}
-
-//Am I a coder?
-var answer = prompt(questions[2]).toLowerCase();
-if(answer != null || answer != ""){
-    if(answer === "y" || answer === "yes"){
-        console.log("Correct answer was submitted");
-        alert('You are right!!!');  
-    }else if(answer === "n" || answer === "no"){
-        console.log("Wrong answer was submitted");
-        alert("Your answer is . . wrong!");
-    }else{
-        console.log("Invalid Answer");
-        alert("Your answer was invalid.");
-    }
-}
-
-//Am I an Amazon Employee?
-var answer = prompt(questions[3]).toLowerCase();
-if(answer != null || answer != ""){
-    if(answer === "y" || answer === "yes"){
-        console.log("Correct answer was submitted");
-        alert('You are right!!!');  
-    }else if(answer === "n" || answer === "no"){
-        console.log("Wrong answer was submitted");
-        alert("Your answer is . . wrong!");
-    }else{
-        console.log("Invalid Answer");
-        alert("Your answer was invalid.");
-    }
-}
-
-//Do I play an instrument?
-var answer = prompt(questions[4]).toLowerCase();
-
-if(answer != null || answer != ""){
-    if(answer === "y" || answer === "yes"){
-        console.log("Correct answer was submitted");
-        alert('You are right!!!');  
-    }else if(answer === "n" || answer === "no"){
-        console.log("Wrong answer was submitted");
-        alert("Your answer is . . wrong!");
-    }else{
-        console.log("Invalid Answer");
-        alert("Your answer was invalid.");
-    }
-}
-*/
+alert("You got " + rightAnswersNum + " out of 7 questions right. ");
