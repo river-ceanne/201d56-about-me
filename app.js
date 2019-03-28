c3f36f7c973ce35d0b1c3dbe5381acf99a40b8d0
idothestamping <douglas.klemp@gmail.com>
2019-03-28 14:16:15 -0700

    saving mid point during refactoring: functions

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 console.log("JS Script linked/started.");
 
-var name;
 var rightAnswersNum = 0;
 var reply;
 var yesReply = ["y","yes","true"];
@@ -15,108 +14,97 @@ var numberTries = 4;
 var numberGuessed = false;
 
 var questions = [
-    "Am I girl?",
-    "Do I like sushi?",
-    "Am I a coder?",
-    "Am I an Amazon employee?",
-    "Do I play an instrument?",
-    "Guess the number I'm thinking:", //4 tries
-    "Guess a country I have visited:" // 6 tries
+  "Am I girl?",
+  "Do I like sushi?",
+  "Am I a coder?",
+  "Am I an Amazon employee?",
+  "Do I play an violin?",
+  "Guess the number I'm thinking:", //4 tries
+  "Guess a country I have visited:" // 6 tries
 ];
 
 var answers = [
-    yesReply,
-    yesReply,
-    yesReply,
-    yesReply,
-    yesReply,
-    "22", // entering single digit <2 considered too high
-    visitedCountries
+  yesReply,
+  yesReply,
+  yesReply,
+  yesReply,
+  noReply,
+  22,
+  visitedCountries
 ];
 
-name = prompt("Let's play a guessing game about me, but first tell me your name.");
-if(name == null || name === ""){
-    name = "anonymous person"
+var userName = prompt("Let's play a guessing game about me, but first tell me your name.");
+if(userName == null || userName === ""){
+  userName = "anonymous person";
 }
-alert("Welcome " + name + "! Let's proceed!");
-question(); // Initiate first function.
+alert("Welcome " + userName + "! Let's proceed!");
 
-// Doug:
-function question(){
-  for(var i = 0; i < 5; i++){
-    reply = prompt(questions[i]).toLowerCase();
-    var counter = 0;
-    var answerArr = answers[i];
-    while(counter < answerArr.length){
-        if(reply === answerArr[counter] || reply === answers[i]){
-            console.log("Correct answer was submitted");
-            alert('You are right!!!');  
-            rightAnswersNum++;
-            break;
-        }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
-            console.log("Wrong answer was submitted");
-            alert("Your answer is . . wrong!");
-        }
-
-        counter++;
+for(var i = 0; i < 5; i++){
+  reply = prompt(questions[i]).toLowerCase();
+  var counter = 0;
+  var answerArr = answers[i];
+  while(counter < answerArr.length){
+    if(reply === answerArr[counter] || reply === answers[i]){
+      correctAnswer();
+      rightAnswersNum++;
+      break;
+    }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
+      wrongAnswer(); 
     }
+    counter++;
   }
-  answerCount();
+}
+answerCount();
+
+function wrongAnswer(){
+  console.log("Wrong answer was submitted");
+  alert("Your answer is . . wrong!");
 }
 
-// Saved for Reference:
-// for(var i = 0; i < 5; i++){
-//     reply = prompt(questions[i]).toLowerCase();
-//     var counter = 0;
-//     var answerArr = answers[i];
-//     while(counter < answerArr.length){
-//         if(reply === answerArr[counter] || reply === answers[i]){
-//             console.log("Correct answer was submitted");
-//             alert('You are right!!!');  
-//             rightAnswersNum++;
-//             break;
-//         }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
-//             console.log("Wrong answer was submitted");
-//             alert("Your answer is . . wrong!");
-//         }
+function correctAnswer(){
+  console.log("Correct answer was submitted");
+  alert('You are right!!!');  
+}
 
-//         counter++;
-//     }
-// }
-
-// Doug:
 function answerCount(){
   while(numberTries != 0){
-    reply = prompt(questions[5]).toLowerCase();
+    reply = parseInt(prompt(questions[5]));
     var answerArr = answers[5];
-        if(reply === answerArr){
-            console.log("Correct answer was submitted");
-            alert('Nice! You are right!!!');  
-            rightAnswersNum++;
-            numberGuessed = true;
-            break;
-        }else if(reply != answerArr){
-            console.log("Wrong answer was submitted");
-            if(reply < answerArr){
-                alert("Your answer is too low! Try Again!");
-            }else if(reply > answerArr){
-                alert("Your answer is too high! Try Again!");
-            }else{
-                alert("Your answer is . . wrong! Invalid response. Try Again!");
-            }
-            
-        }
+    if(reply === answerArr){
+      correctAnswer();
+      rightAnswersNum++;
+      numberGuessed = true;
+      break;
+    }else if(reply != answerArr){
+      console.log("Wrong answer was submitted");
+      replyHighOrLow(reply, answerArr);
+      
+    }
 
     if(numberGuessed){
-        break;
+      break;
     }
 
     numberTries--;
-    if(numberTries == 0){
-        alert("You get no more tries for this question! Sorry T_T");
-    }
+    checkNumTries(numberTries);
   }
   countryQuestion();
+}
+
+function replyHighOrLow(reply, answerArr){
+  if(reply < answerArr){
+    alert("Your answer is too low! Try Again!");
+  }else if(reply > answerArr){
+    alert("Your answer is too high! Try Again!");
+  }else{
+    alert("Your answer is . . wrong! Invalid response. Try Again!");
+  }
+}
+
+function checkNumTries(){
+  if(numberTries == 0){
+    alert("You get no more tries for this question! Sorry T_T");
+  }
 }
 
 // Saved for Reference:
@@ -158,29 +146,25 @@ function countryQuestion(){
     var counter = 0;
     var answerArr = answers[6];
     while(counter < answerArr.length){
-        if(reply === answerArr[counter] || reply === answers[5]){
-            console.log("Correct answer was submitted");
-            alert('You are right!!!');  
-            rightAnswersNum++;
-            countryGuessed = true;
-            break;
-        }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
-            console.log("Wrong answer was submitted");
-            alert("Your answer is . . wrong! Try again!");
-        }
+      if(reply === answerArr[counter] || reply === answers[5]){
+        console.log("Correct answer was submitted");
+        alert('You are right!!!');  
+        rightAnswersNum++;
+        countryGuessed = true;
+        break;
+      }else if(counter == answerArr.length - 1 && reply != answerArr[counter]){
+        wrongAnswer(); 
+      }
 
-        counter++;
+      counter++;
     }
 
     if(countryGuessed){
-        break;
+      break;
     }
 
     countryTries--;
-    if(countryTries == 0){
-        alert("You get no more tries for this question! Sorry T_T");
-        break;
-    }
+    checkNumTries(countryTries);
   }
 }
 
